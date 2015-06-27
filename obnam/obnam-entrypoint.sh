111724fb6b5c41cd2e56b7554ca8b93c74e1fd7b
Vincent Demeester <vincent@sbr.pm>
2015-06-27 22:06:52 +0200

    Update entrypoint to handle config file binding

--- obnam/obnam-entrypoint.sh
+++ obnam/obnam-entrypoint.sh
@@ -3,16 +3,26 @@
 
 DESTINATION="/obnam/destination"
 SOURCE="/obnam/source"
+CONFIG="/obnam/config"
+
+OBNAM_OPTS=""
 
 if test -n "$1"; then
     DESTINATION=$1
     shift
 fi
-# TODO check if source is empty
+
 if ! test "$(ls -A ${SOURCE})"; then
     echo "$SOURCE folder is empty, skipping backup"
     exit 1
 fi
 
-echo "Backup ${SOURCE} to ${DESTINATION}"
-obnam backup -r $DESTINATION $SOURCE
+if test -r ${CONFIG}; then
+    echo "Use ${CONFIG} as configuration"
+    OBNAM_OPTS="${OBNAM_OPTS} --config=${CONFIG} $SOURCE"
+else
+    OBNAM_OPTS=" -r $DESTINATION $SOURCE"
+fi
+
+echo "Backup using ${OBNAM_OPTS}"
+obnam backup $OBNAM_OPTS
