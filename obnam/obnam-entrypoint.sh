a797301a6b1bb9df4520cc77170208b437fa1cb1
Vincent Demeester <vincent@sbr.pm>
2015-06-27 14:09:51 +0200

    Update obnam-entrypoint to be a little more flexible

--- obnam/obnam-entrypoint.sh
+++ obnam/obnam-entrypoint.sh
@@ -4,6 +4,15 @@
 DESTINATION="/obnam/destination"
 SOURCE="/obnam/source"
 
+if test -n "$1"; then
+    DESTINATION=$1
+    shift
+fi
 # TODO check if source is empty
+if ! test "$(ls -A ${SOURCE})"; then
+    echo "$SOURCE folder is empty, skipping backup"
+    exit 1
+fi
 
-obnam -r /obnam/destination /obnam/source
+echo "Backup ${SOURCE} to ${DESTINATION}"
+obnam backup -r $DESTINATION $SOURCE
