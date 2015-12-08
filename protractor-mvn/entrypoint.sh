ef1d60b53ae875f95066f2321a9e82e6a6b02217
Vincent Demeester <vincent@sbr.pm>
2015-12-08 14:45:01 +0100

    Ugly entrypoint

--- protractor-mvn/entrypoint.sh
+++ protractor-mvn/entrypoint.sh
@@ -2,4 +2,13 @@
 
 source /etc/environment
 
+export HTTP_PROXY=$HTTP_PROXY
+export HTTPS_PROXY=$HTTPS_PROXY
+export FTP_PROXY=$FTP_PROXY
+export NO_PROXY=$NO_PROXY
+export http_proxy=$http_proxy
+export https_proxy=$https_proxy
+export ftp_proxy=$ftp_proxy
+export no_proxy=$no_proxy
+
 exec "$@"
